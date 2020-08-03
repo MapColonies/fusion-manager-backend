04862561f3a21417e307f071363dd1ad4c36a161
shimoncohen <shimoncohen655@gmail.com>
2020-08-03 16:42:51 +0300

    Added search_pathto returned items.

--- geefusion_project_server/imagery/serializers.py
+++ geefusion_project_server/imagery/serializers.py
@@ -1,50 +1,41 @@
 from rest_framework import serializers
 from .models import Project, Resource, Mask
 from utils.mask import mask_to_json
-
-# class MaskSerializer(serializers.ModelSerializer):
-    
-#     class Meta:
-#         model = Mask
-#         fields = ('no_mask', 'feather', 'mode', 'band', 'fill_value', 
-#                     'threshold', 'hole_size', 'white_fill', 'no_data')
+from utils.path import change_root_dir
+from utils.constants.extensions import get_project_extension, get_resource_extension
 
 class ResourceSerializer(serializers.ModelSerializer):
     taken_at = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%SZ')
     mask = serializers.SerializerMethodField()
+    search_path = serializers.SerializerMethodField()
     
     class Meta:
         model = Resource
-        fields = ('name', 'version', 'extent', 'taken_at', 'level', 'resolution', 'mask')
+        fields = ('name', 'version', 'search_path', 'extent', 'taken_at', 'level', 'resolution', 'mask')
         depth = 1
     
     def get_mask(self, obj):
         mask = obj.mask
         return mask_to_json(mask)
-
-        # if mask.no_mask:
-        #     return { 'no_mask': True }
-        
-        # return {
-        #     'feather': mask.feather,
-        #     'mode': mask.mode,
-        #     'band': mask.band,
-        #     'fill_value': mask.fill_value,
-        #     'threshold': mask.threshold,
-        #     'hole_size': mask.hole_size,
-        #     'white_fill': mask.white_fill,
-        #     'no_data': mask.no_data
-        # }
+    
+    def get_search_path(self, obj):
+        extension = get_resource_extension()
+        return change_root_dir(obj.path, 'Imagery')[:-len(extension)]
 
 class ProjectSerializer(serializers.ModelSerializer):
     resources = ResourceSerializer(many=True)
     version = serializers.SerializerMethodField()
+    search_path = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
-        fields = ('name', 'version', 'resources')
+        fields = ('name', 'version', 'search_path', 'resources')
         depth = 2
     
     def get_version(self, obj):
         version = obj.version
         return version if version != 0 else None
+    
+    def get_search_path(self, obj):
+        extension = get_project_extension()
+        return change_root_dir(obj.path, 'Imagery')[:-len(extension)]
